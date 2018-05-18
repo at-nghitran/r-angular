eb97008263028d144147008b7320d470df2c0e6f
at-nghitran <nghi.tran@asiantech.vn>
2018-05-18 11:27:58 +0700

    update get list service

--- src/app/services/getList/get-list.service.ts
+++ src/app/services/getList/get-list.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, of, from } from 'rxjs';
-import { Response } from '@angular/http';
-import { filter, map, flatMap, toArray } from 'rxjs/operators';
+import { filter } from 'rxjs/operators';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Employee } from '../../employee';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +10,6 @@ import { Employee } from '../../employee';
 export class GetListService {
 
   urlList = 'assets/data/member.json';
-  Emp: Employee;
-  private employee: Observable<Employee[]>;
 
   constructor(private http: HttpClient) { }
 
@@ -26,7 +22,7 @@ export class GetListService {
   }
 
   getSameMember (employeeList, employee) {
-    let arrayMember = employeeList.filter(mem => mem['position'] === employee['position']);
+    const arrayMember = employeeList.filter(mem => mem['position'] === employee['position']);
     const index = arrayMember.findIndex(el => el.id === employee.id);
     arrayMember.splice(index, 1);
     return arrayMember;
