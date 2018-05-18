6a68b2ba68c4b2c1f1b96e0d73845fb9455999e7
at-nghitran <nghi.tran@asiantech.vn>
2018-05-18 14:33:21 +0700

    bai 1 validation email and password

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  email: string;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit (myForm: NgForm) {
+    alert(`ban da dang nhap thanh cong voi email: ${this.email}`);
   }
 
 }
